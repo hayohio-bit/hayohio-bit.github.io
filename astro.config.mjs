f374d16f178b644fdaaf76060be7bbb7a85633ac
하영 <hayohio@gmail.com>
2025-12-01 01:48:24 +0900

    2025-12-01

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://hayohio-bit.github.io",
-  base: '/superS.H.Y-.git',
+  base: '/learn-dev-run',
   integrations: [mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],

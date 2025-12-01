f9b219e28970c4c56ee3ed47c8291901e2b0796b
하영 <hayohio@gmail.com>
2025-12-02 00:46:37 +0900

    2025-12-02

---
+++ src/pages/search.json.js
@@ -0,0 +1,17 @@
+import { getCollection } from "astro:content";
+
+export async function GET({ }) {
+    const posts = await getCollection("blog");
+    const searchList = posts.map((post) => ({
+        title: post.data.title,
+        description: post.data.description,
+        slug: post.slug,
+        pubDate: post.data.pubDate,
+    }));
+
+    return new Response(JSON.stringify(searchList), {
+        headers: {
+            "content-type": "application/json",
+        },
+    });
+}
